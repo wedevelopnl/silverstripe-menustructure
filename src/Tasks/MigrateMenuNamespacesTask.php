bcf57eab69a81c769881f046e167a0e150b414dc
Dennis Prins <dennis@wedevelop.nl>
2024-10-17 11:11:41 +0200

    MigrateMenuNamespacesTask

---
+++ src/Tasks/MigrateMenuNamespacesTask.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace WeDevelop\Menustructure\Tasks;
+
+use SilverStripe\Dev\BuildTask;
+use WeDevelop\Menustructure\Model\Menu;
+use WeDevelop\Menustructure\Model\MenuItem;
+
+class MigrateMenuNamespacesTask extends BuildTask
+{
+    protected string $title = 'Migrate menu namespaces';
+    private static string $segment = 'migrate-menu-namespaces';
+    protected string $description = 'Migrate menu (items) namespaces from old to new namespace';
+
+    public function run($request)
+    {
+        $menus = Menu::get();
+        $menuItems = MenuItem::get();
+
+        foreach ($menus as $menu) {
+            $menu->ClassName = str_replace('TheWebmen', 'WeDevelop', $menu->ClassName);
+            $menu->write();
+        }
+
+        foreach ($menuItems as $menuItem) {
+            $menuItem->ClassName = str_replace('TheWebmen', 'WeDevelop', $menuItem->ClassName);
+            $menuItem->write();
+        }
+    }
+}
